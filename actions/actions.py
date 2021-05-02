1c1945e8cc49f29badd92c5ac73f6c46d1d95238
amititme68 <amititme68@gmail.com>
2021-05-02 15:14:00 +0530

    Created gitignore file

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,25 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from datetime import datetime
+import pytz
+def CheckAvailablityTime():
+    UTC=pytz.utc
+    IST=pytz.timezone('Asia/Kolkata')
+    datetime_ist=datetime.now(IST)
+    check_new = 0
+    check = datetime_ist.strftime('%H')
+    if (check[0] == '0'):
+        check_new = check[1]
+    else:
+        check_new = int(check)
+    if ((check_new >= 7 and check_new < 10) or (check_new >= 19 and check_new < 22)):
+        return 1
+    else:
+        return 0
+    print(check_new)
+
+n=CheckAvailablityTime()
+if(n==1):
+    return(time())
